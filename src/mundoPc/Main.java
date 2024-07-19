4d27858bab4c3dd6f5b7885f9fa6d5aa141452a0
Fernando <info_fotografia@icloud.com>
2024-07-19 13:25:06 +0200

    Creación clase Monitor

--- src/mundoPc/Main.java
+++ src/mundoPc/Main.java
@@ -1,9 +1,18 @@
 package mundoPc;
+import mundoPc.modelo.Monitor;
+import mundoPc.modelo.Raton ;
+import mundoPc.modelo.Teclado;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //Crear objetos
+
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
+
     }
 }
